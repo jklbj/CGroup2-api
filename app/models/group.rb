9293d148898d22262f9d4b0d19b1ed36186d71ed
KevinYang515 <kevin850515123@gmail.com>
2019-04-23 13:37:06 +0800

    Finish model and api/group_events

--- app/models/group.rb
+++ app/models/group.rb
@@ -12,6 +12,63 @@ module CGroup2
         plugin :whitelist_security
         set_allowed_columns :title, :description, :member_id, :limit_number, :due_at, :event_start_at, :event_end_at
 
+        # Secure getters and setters
+        def title
+            SecureDB.decrypt(title_secure)
+        end
+    
+        def title=(plaintext)
+            self.title_secure = SecureDB.encrypt(plaintext)
+        end
+
+        def description
+            SecureDB.decrypt(description_secure)
+        end
+    
+        def description=(plaintext)
+            self.description_secure = SecureDB.encrypt(plaintext)
+        end
+
+        def member_id
+            SecureDB.decrypt(member_id_secure)
+        end
+    
+        def member_id=(plaintext)
+            self.member_id_secure = SecureDB.encrypt(plaintext)
+        end
+
+        def limit_number
+            SecureDB.decrypt(limit_number_secure)
+        end
+    
+        def limit_number=(plaintext)
+            self.limit_number_secure = SecureDB.encrypt(plaintext)
+        end
+
+        def due_at
+            SecureDB.decrypt(due_at_secure)
+        end
+    
+        def due_at=(plaintext)
+            self.due_at_secure = SecureDB.encrypt(plaintext)
+        end
+
+        def event_start_at
+            SecureDB.decrypt(event_start_at_secure)
+        end
+    
+        def event_start_at=(plaintext)
+            self.event_start_at_secure = SecureDB.encrypt(plaintext)
+        end
+
+        def event_end_at
+            SecureDB.decrypt(event_end_at_secure)
+        end
+    
+        def event_end_at=(plaintext)
+            self.event_end_at_secure = SecureDB.encrypt(plaintext)
+        end
+
         # rubocop:disable MethodLength
         def to_json(options = {})
             JSON(
