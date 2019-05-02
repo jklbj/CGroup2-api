d7aae8c2404f691f8267c62d581a395d12a80a89
KevinYang515 <kevin850515123@gmail.com>
2019-05-02 15:47:35 +0800

    Finish part I

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,15 +2,25 @@
 
 require 'json'
 require 'sequel'
+require_relative './password'
 
 module CGroup2
   # Models a project
   class account < Sequel::Model
-    one_to_many :groups
+    one_to_many :groups,
     one_to_many :calendars
     plugin :association_dependencies, groups: :destroy, calendars: :destroy
 
-    plugin :timestamps
+    plugin :timestamps, update_on_create: true
+
+    def password=(new_password)
+      self.password_digest = Password.digest(new_password)
+    end
+
+    def password?(try_password)
+      digest = Credence::Password.from_digest(password_digest)
+      digest.correct?(try_password)
+    end 
 
     # rubocop:disable MethodLength
     def to_json(options = {})
